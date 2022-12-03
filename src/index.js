ae7269a8e58906622b9f7c559136bc66cf2bb2d1
DawidKrupski <dawid.krupski323@gmail.com>
2022-12-03 01:21:59 +0100

    add files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+
+const input = document.querySelector('input#search-box');
 
 const DEBOUNCE_DELAY = 300;
+
+const selectCountries = event => {
+  const countryRemove = document
+    .querySelectorAll('ul.country-list>div')
+    .forEach(country => country.remove());
+  fetchCountries(event.target.value);
+};
+
+input.addEventListener('input', debounce(selectCountries, DEBOUNCE_DELAY));
